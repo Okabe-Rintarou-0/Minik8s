e4c7da55c97281f32bd46017b7fac9dd72eb8ebb
Vectorxj <Vectorxj@outlook.com>
2022-04-30 18:44:15 +0800

    add apiobject service

---
+++ apiObject/service.go
@@ -0,0 +1,27 @@
+package apiObject
+
+type ServicePort struct {
+	Name       string `yaml:"name"`
+	Port       int32  `yaml:"port,omitempty"`
+	TargetPort int32  `yaml:"targetPort,omitempty"`
+}
+
+type ServiceSpec struct {
+	Type      string            `yaml:"type,omitempty"`
+	Ports     []ServicePort     `yaml:"ports,omitempty"`
+	Selector  map[string]string `yaml:"selector,omitempty"`
+	ClusterIP string            `yaml:"clusterIP,omitempty"`
+}
+
+type ServiceStatus struct {
+}
+
+type Service struct {
+	ApiObjectBase `yaml:",inline"`
+	Spec          ServiceSpec   `yaml:"spec"`
+	Status        ServiceStatus `yaml:"status"`
+}
+
+func (service *Service) FullName() string {
+	return service.Metadata.Name + "_" + service.Metadata.Namespace
+}
