70f230758ccf940439fc95416c0d20b9ae5b2a07
okabe <923048992@qq.com>
2022-05-25 14:27:43 +0800

    fix: label status should also be updated

--- apiserver/src/handlers/label.go
+++ apiserver/src/handlers/label.go
@@ -7,6 +7,7 @@ import (
 	"minik8s/apiObject"
 	"minik8s/apiserver/src/etcd"
 	"minik8s/apiserver/src/url"
+	"minik8s/entity"
 	"net/http"
 	"path"
 	"strconv"
@@ -30,10 +31,10 @@ func HandleLabelNode(c *gin.Context) {
 	log("Add labels %v to node[hostname = %v]", labels, name)
 
 	node := apiObject.Node{}
-	etcdURL := path.Join(url.NodeURL, namespace, name)
+	etcdNodeURL := path.Join(url.NodeURL, namespace, name)
 	var err error
 	var raw string
-	if raw, err = etcd.Get(etcdURL); err == nil {
+	if raw, err = etcd.Get(etcdNodeURL); err == nil {
 		log("got %s from etcd", raw)
 		if err = json.Unmarshal([]byte(raw), &node); err == nil {
 			nodeLabels := node.Labels()
@@ -46,8 +47,18 @@ func HandleLabelNode(c *gin.Context) {
 				}
 			}
 			nodeJson, _ := json.Marshal(node)
-			if err = etcd.Put(etcdURL, string(nodeJson)); err == nil {
+			if err = etcd.Put(etcdNodeURL, string(nodeJson)); err == nil {
 				c.String(http.StatusOK, "ok")
+
+				etcdNodeStatusURL := path.Join(url.NodeURL, "status", node.Namespace(), node.Name())
+				if nodeStatusStr, err := etcd.Get(etcdNodeStatusURL); err == nil {
+					nodeStatus := entity.NodeStatus{}
+					if err = json.Unmarshal([]byte(nodeStatusStr), &nodeStatus); err == nil {
+						nodeStatus.Labels = nodeLabels.DeepCopy()
+						nodeStatusJson, _ := json.Marshal(nodeStatus)
+						_ = etcd.Put(etcdNodeStatusURL, string(nodeStatusJson))
+					}
+				}
 				return
 			}
 		}
