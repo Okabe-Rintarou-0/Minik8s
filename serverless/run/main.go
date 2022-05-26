843b7c188bcdd66ee25606e687ec03410f2a5985
okabe <923048992@qq.com>
2022-05-26 09:33:07 +0800

    code: test current dir

--- serverless/run/main.go
+++ serverless/run/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"minik8s/serverless/src/function"
 	"minik8s/serverless/src/knative"
 	"minik8s/serverless/src/registry"
+	"os/exec"
 )
 
 func main() {
-	registry.InitRegistry()                                   // only run on master node
+	registry.InitRegistry()
+	// only run on master node
+	cmd := exec.Command("ls")
+	output, _ := cmd.Output()
+	fmt.Println(output)
 	function.InitFunction("helloworld", "../src/app/func.py") // the third parameter need to be replaced
 	kn := knative.NewKnative()
 	kn.Run()
