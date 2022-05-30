c2fb03b2d43a871eebe21c8754886f44f972e0ca
okabe <923048992@qq.com>
2022-05-30 19:53:13 +0800

    add autoscaler testcase

---
+++ testcases/autoscaler/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+var bgCtx = context.Background()
+var cancel context.CancelFunc
+
+func consumeHigherCpu(ctx context.Context) {
+	for i := 0; i < 20; i++ {
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+			}
+		}()
+	}
+}
+
+func higher(c *gin.Context) {
+	if cancel != nil {
+		return
+	}
+
+	var ctx context.Context
+	ctx, cancel = context.WithCancel(bgCtx)
+	consumeHigherCpu(ctx)
+	c.String(http.StatusOK, "higher cpu utilization!")
+}
+
+func lower(c *gin.Context) {
+	if cancel != nil {
+		cancel()
+	}
+	c.String(http.StatusOK, "lower cpu utilization!")
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/higher", higher)
+	r.GET("/lower", lower)
+	log.Fatal(r.Run(":8080"))
+}
